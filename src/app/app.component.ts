f1602d4759608c2225523f658964856f460b9f58
Davide Di Francesco <davdifr@outlook.com>
2024-05-07 11:46:51 +0200

    Testing WebStorm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,14 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { WelcomeToAngularComponent } from './components/welcome-to-angular/welcome-to-angular.component';
-import { ButtonClickComponent } from './components/button-click/button-click.component';
-import { TwoWayBindingComponent } from './components/two-way-binding/two-way-binding.component';
-import { RouterModule } from '@angular/router';
-import { HttpRequestComponent } from './components/http-request/http-request.component';
+import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { HttpRequestComponent } from "./components/http-request/http-request.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterModule, HttpRequestComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+    selector: "app-root",
+    standalone: true,
+    imports: [RouterModule, HttpRequestComponent],
+    templateUrl: "./app.component.html",
+    styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = '05-angular-bootcamp-routing';
+    title = "05-angular-bootcamp-routing";
 }
